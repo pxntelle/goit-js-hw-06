f42e2b4f545f088eebffbd440fc3c0ed76387851
pxntelle <taniapantelle@gmail.com>
2023-05-20 20:00:35 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeColorBtn = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  textColor.textContent = color;
+  body.style.backgroundColor = color;
+}
