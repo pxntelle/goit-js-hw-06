9c4f3cd031fbf0dc641689d48cf5f8ff673eec22
pxntelle <taniapantelle@gmail.com>
2023-05-20 19:14:19 +0200

    created

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryElements = category.querySelectorAll("li").length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryElements}`);
+});
