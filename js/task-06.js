65546e660fa75bbc49b80217879ef2d649e0b490
pxntelle <taniapantelle@gmail.com>
2023-05-26 13:07:03 +0200

    fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,26 +1,26 @@
-const validationInput = document.querySelector('#validation-input');
+const validationInput = document.querySelector("#validation-input");
 
-validationInput.addEventListener('blur', () => {
+validationInput.addEventListener("blur", () => {
   const inputLength = validationInput.value.length;
-  const dataLength = Number(validationInput.getAttribute('data-length'));
+  const dataLength = Number(validationInput.getAttribute("data-length"));
 
-//   if (inputLength === dataLength) {
-//     validationInput.classList.remove('invalid');
-//     validationInput.classList.add('valid');
-//   } else {
-//     validationInput.classList.remove('valid');
-//     validationInput.classList.add('invalid');
-//   }
-// });
+  //   if (inputLength === dataLength) {
+  //     validationInput.classList.remove('invalid');
+  //     validationInput.classList.add('valid');
+  //   } else {
+  //     validationInput.classList.remove('valid');
+  //     validationInput.classList.add('invalid');
+  //   }
+  // });
 
-if (inputLength === dataLength) {
-  setValidationClass('valid', 'invalid');
-} else {
-  setValidationClass('invalid', 'valid');
-}
-}
+  if (inputLength === dataLength) {
+    setValidationClass("valid", "invalid");
+  } else {
+    setValidationClass("invalid", "valid");
+  }
+});
 
 function setValidationClass(classToAdd, classToRemove) {
-validationInput.classList.remove(classToRemove);
-validationInput.classList.add(classToAdd);
-});
+  validationInput.classList.remove(classToRemove);
+  validationInput.classList.add(classToAdd);
+}
